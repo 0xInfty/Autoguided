eca1537f5c8242fbcda4e1203e0e7a89264f52ff
0xInfty <anirix.vall@gmail.com>
2025-03-03 17:29:35 +0000

    Modified only the header comment [m1]

--- ToyExample/torch_utils/persistence.py
+++ ToyExample/torch_utils/persistence.py
@@ -1,9 +1,8 @@
-# Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# This is a commented copy from code found at "EDM2 and Autoguidance" by Tero Karras et al
+# https://github.com/NVlabs/edm2/blob/main/torch_utils/persistence.py licensed under CC BY-NC-SA 4.0
 #
-# This work is licensed under a Creative Commons
-# Attribution-NonCommercial-ShareAlike 4.0 International License.
-# You should have received a copy of the license along with this
-# work. If not, see http://creativecommons.org/licenses/by-nc-sa/4.0/
+# Original copyright disclaimer:
+# Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
 """Facilities for pickling Python code alongside other data.
 
