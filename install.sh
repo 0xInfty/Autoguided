5cb36fbcb3342f6d18fb729d6134de74290c23c1
0xInfty <anirix.vall@gmail.com>
2025-04-15 12:17:38 +0100

    No pip cache [D1]

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 pip install torch torchvision torchaudio \
     huggingface_hub==0.25.2 diffusers==0.26.3 accelerate==0.27.2 \
     matplotlib click tqdm colorlog \
-    pyvtools pyvdirs --no-cache-dir
+    pyvtools pyvdirs
