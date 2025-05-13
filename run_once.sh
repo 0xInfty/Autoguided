27ee221aead3f5fcf1c6d494c2c5fd81828d61c3
0xInfty <anirix.vall@gmail.com>
2025-05-13 19:01:20 +0100

    Bash variables to define paths [M1]

--- run_once.sh
+++ run_once.sh
@@ -1,3 +1,5 @@
-python ToyExample/toy_example.py train --guidance --guide-path "ToyExample/00_PreTrained/Ref/iter0512.pkl" --invert --acid --outdir "ToyExample/ACID" --seed 0 --val --test --verbose --logging "ToyExample/log_ACID.txt"
-python ToyExample/toy_example.py train --guidance --guide-path "ToyExample/00_PreTrained/Ref/iter0512.pkl" --no-acid --outdir "ToyExample/NoACID" --seed 0 --val --test --verbose --logging "ToyExample/log_NoACID.txt"
-# python ToyExample/toy_example.py train --guidance --guide-path "ToyExample/00_PreTrained/Ref/iter0512.pkl" --invert --acid --interpol --outdir "ToyExample/ACIDInterpol" --seed 0 --val --test --verbose --logging "ToyExample/log_ACIDInterpol.txt"
+ref="/00_PreTrained"; subdir=""
+
+python ToyExample/toy_example.py train --guidance --guide-path "ToyExample"$ref"/Ref/iter0512.pkl" --invert --acid --outdir "ToyExample"$subdir"/ACID" --seed 0 --val --test --verbose --logging "ToyExample"$subdir"/log_ACID.txt"
+python ToyExample/toy_example.py train --guidance --guide-path "ToyExample"$ref"/Ref/iter0512.pkl"  --no-acid --outdir "ToyExample"$subdir"/NoACID" --seed 0 --val --test --verbose --logging "ToyExample"$subdir"/log_NoACID.txt"
+# python ToyExample/toy_example.py train --guidance --guide-path "ToyExample"$ref"/Ref/iter0512.pkl"  --invert --acid --interpol --outdir "ToyExample"$subdir"/ACIDInterpol" --seed 0 --val --test --verbose --logging "ToyExample"$subdir"/log_ACIDInterpol.txt"
