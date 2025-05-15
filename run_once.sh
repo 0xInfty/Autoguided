0ed05854a735221cd8e2a0cadabcfd5c5dca175c
0xInfty <anirix.vall@gmail.com>
2025-05-15 10:45:30 +0100

    New lines [m1]

--- run_once.sh
+++ run_once.sh
@@ -1,4 +1,4 @@
-ref="/00_PreTrained"; subdir="/23NormalizedLogits"
+ref="/00_PreTrained"; subdir="/23_NormalizedLogits"
 
 python ToyExample/toy_example.py train --guidance --guide-path "ToyExample"$ref"/Ref/iter0512.pkl" --invert --acid --outdir "ToyExample"$subdir"/ACID" --seed 0 --val --test --verbose --logging "ToyExample"$subdir"/log_ACID.txt"
 python ToyExample/toy_example.py train --guidance --guide-path "ToyExample"$ref"/Ref/iter0512.pkl" --acid --outdir "ToyExample"$subdir"/ACIDNonInverted" --seed 0 --val --test --verbose --logging "ToyExample"$subdir"/log_ACIDNonInverted.txt"
@@ -11,3 +11,12 @@ python ToyExample/toy_example.py train --guidance --guide-path "ToyExample"$ref"
 
 python ToyExample/toy_example.py train --guidance --guide-path "ToyExample"$ref"/Ref/iter0512.pkl"  --no-acid --outdir "ToyExample"$subdir"/NoACID" --seed 0 --val --test --verbose --logging "ToyExample"$subdir"/log_NoACID.txt"
 python ToyExample/toy_example.py train --guidance --guide-path "ToyExample"$ref"/Ref/iter0512.pkl"  --invert --acid --interpol --outdir "ToyExample"$subdir"/ACIDInterpol" --seed 0 --val --test --verbose --logging "ToyExample"$subdir"/log_ACIDInterpol.txt"
+
+python ToyExample/toy_example.py train --guidance --guide-path "ToyExample"$ref"/Ref/iter0512.pkl" --invert --interpol --acid --outdir "ToyExample"$subdir"/InterpolACID" --seed 0 --val --test --verbose --logging "ToyExample"$subdir"/log_InterpolACID.txt" --device $device
+python ToyExample/toy_example.py train --guidance --guide-path "ToyExample"$ref"/Ref/iter0512.pkl" --interpol --acid --outdir "ToyExample"$subdir"/InterpolACIDNonInverted" --seed 0 --val --test --verbose --logging "ToyExample"$subdir"/log_InterpolACIDNonInverted.txt" --device $device
+
+python ToyExample/toy_example.py train --guidance --guide-path "ToyExample"$ref"/Ref/iter0512.pkl" --early --invert --interpol --acid --outdir "ToyExample"$subdir"/EarlyInterpolACID" --seed 0 --val --test --verbose --logging "ToyExample"$subdir"/log_EarlyInterpolACID.txt" --device $device
+python ToyExample/toy_example.py train --guidance --guide-path "ToyExample"$ref"/Ref/iter0512.pkl" --early --interpol --acid --outdir "ToyExample"$subdir"/EarlyInterpolACIDNonInverted" --seed 0 --val --test --verbose --logging "ToyExample"$subdir"/log_EarlyInterpolACIDNonInverted.txt" --device $device
+
+python ToyExample/toy_example.py train --guidance --guide-path "ToyExample"$ref"/Ref/iter0512.pkl" --late --invert --interpol --acid --outdir "ToyExample"$subdir"/LateInterpolACID" --seed 0 --val --test --verbose --logging "ToyExample"$subdir"/log_LateInterpolACID.txt" --device $device
+python ToyExample/toy_example.py train --guidance --guide-path "ToyExample"$ref"/Ref/iter0512.pkl" --late --interpol --acid --outdir "ToyExample"$subdir"/LateInterpolACIDNonInverted" --seed 0 --val --test --verbose --logging "ToyExample"$subdir"/log_LateInterpolACIDNonInverted.txt" --device $device
