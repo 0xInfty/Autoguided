67c291cad496b5f14edda4ab2855899143521bd4
0xInfty <anirix.vall@gmail.com>
2025-06-26 19:20:49 +0100

    Test script using data selection [m1]

--- Images/train/test.sh
+++ Images/train/test.sh
@@ -3,4 +3,8 @@ subdir="01_CIFAR10/test"; ref="network-snapshot-0025165-0.050.pkl"
 batchsize=512
 devices=1,2; ndevices=2
 
-CUDA_VISIBLE_DEVICES=$devices torchrun --standalone --nproc_per_node=$ndevices Images/train_edm2.py --outdir=$subdir --dataset $dataset --preset=$preset --batch-gpu=$batchsize
+# With no data selection
+# CUDA_VISIBLE_DEVICES=$devices torchrun --standalone --nproc_per_node=$ndevices Images/train_edm2.py --outdir=$subdir --dataset $dataset --preset=$preset --batch-gpu=$batchsize
+
+# With data selection
+CUDA_VISIBLE_DEVICES=$devices torchrun --standalone --nproc_per_node=$ndevices Images/train_edm2.py --outdir=$subdir --dataset $dataset --preset=$preset --batch-gpu=$batchsize --acid --guide-path $ref
