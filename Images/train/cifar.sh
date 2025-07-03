38837e536a3280b3543e8d6c217ffb34ad557ada
0xInfty <anirix.vall@gmail.com>
2025-07-03 20:50:05 +0100

    Changed AJEST ref for CIFAR10 [m1]

--- Images/train/cifar.sh
+++ Images/train/cifar.sh
@@ -1,13 +1,13 @@
 preset="edm2-cifar10-xxs"; dataset="cifar10"
-subdir="01_CIFAR10/Ref/00"; ref="01_CIFAR10/Ref/network-snapshot-0033554-0.100.pkl"
+subdir="01_CIFAR10/AJEST/00"; ref="01_CIFAR10/Ref/00/network-snapshot-0000639-0.100.pkl"
 batchsize=512
 devices=1,2; ndevices=2
 device=2
 
 # With no data selection
-CUDA_VISIBLE_DEVICES=$devices torchrun --standalone --nproc_per_node=$ndevices Images/train_edm2.py --outdir=$subdir --dataset $dataset --preset=$preset --batch-gpu=$batchsize
+# CUDA_VISIBLE_DEVICES=$devices torchrun --standalone --nproc_per_node=$ndevices Images/train_edm2.py --outdir=$subdir --dataset $dataset --preset=$preset --batch-gpu=$batchsize
 # CUDA_VISIBLE_DEVICES=$device python Images/train_edm2.py  --outdir=$subdir --dataset $dataset --preset=$preset --batch-gpu=$batchsize
 
 # With data selection
-# CUDA_VISIBLE_DEVICES=$devices torchrun --standalone --nproc_per_node=$ndevices Images/train_edm2.py --outdir=$subdir --dataset $dataset --preset=$preset --batch-gpu=$batchsize --acid --guide-path $ref
+CUDA_VISIBLE_DEVICES=$devices torchrun --standalone --nproc_per_node=$ndevices Images/train_edm2.py --outdir=$subdir --dataset $dataset --preset=$preset --batch-gpu=$batchsize --acid --guide-path $ref
 # CUDA_VISIBLE_DEVICES=$device python Images/train_edm2.py --outdir=$subdir --dataset $dataset --preset=$preset --batch-gpu=$batchsize --acid --guide-path $ref
