56f4d8c793208d8c547338527faa10693ca5c270
0xInfty <anirix.vall@gmail.com>
2025-07-23 17:29:05 +0100

    Just changed default for device [m1]

--- ToyExample/train/test.sh
+++ ToyExample/train/test.sh
@@ -1,3 +1,3 @@
-ref="/00_PreTrained"; subdir="/99_Test"; device=2
+ref="/00_PreTrained"; subdir="/99_Test"; device=0
 
 python ToyExample/toy_example.py train --outdir "ToyExample"$subdir --dim 32 --total-iter 128 --guidance --guide-path "ToyExample/"$ref"/Ref/iter0512.pkl" --invert --acid --seed 0 --val --test --verbose --logging "ToyExample"$subdir"/log.txt" --device $device
