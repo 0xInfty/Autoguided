1656920dd55ac1e8f5846439775f3ed7eaf48d26
0xInfty <anirix.vall@gmail.com>
2025-07-23 15:48:12 +0100

    Added missing packages [m1]

--- install.sh
+++ install.sh
@@ -3,11 +3,13 @@
 # Linux
 pip install torch torchvision torchaudio \
     huggingface_hub==0.25.2 diffusers==0.26.3 accelerate==0.27.2 \
+    datasets wandb \
     matplotlib click tqdm colorlog \
-    pyvtools pyvdirs --no-cache
+    pyvtools pyvtorch pyvdirs --no-cache
 
 # Windows
 # pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
 # pip install huggingface_hub==0.25.2 diffusers==0.26.3 accelerate==0.27.2 \
 #     matplotlib click tqdm colorlog \
-#     pyvtools pyvdirs --no-cache
+#     datasets wandb \
+#     pyvtools pyvtorch pyvdirs --no-cache
