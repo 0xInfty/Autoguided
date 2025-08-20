6d90f1b8f2fb1cdef5f3c2ba11f863fee09a75cb
0xInfty <anirix.vall@gmail.com>
2025-08-20 19:24:10 +0100

    Implementation + Chosen EMAs list debug [m1+D1]
    Minor:
    	decoupled the `calculate_metrics_for_checkpoints` function, in case I ever need to run for a series of specific checkpoints, not necessarily from the same run/folder
    
    Debug:
    	hopefully fixed the `emas` list Click argument

--- Images/get_validation_metrics.py
+++ Images/get_validation_metrics.py
@@ -468,10 +468,10 @@ def calculate_metrics_for_generated_images(super_dir, fd_metrics=True,
     return results
 
 #----------------------------------------------------------------------------
-# Calculate metrics for all stored models as a post-hoc validation curve
+# Calculate metrics for a series of models
 
 def calculate_metrics_for_checkpoints(
-        checkpoints_dir,
+        checkpoint_filepaths,
         dataset_name = "tiny",          # Dataset used for training.
         ref_path = None,                # Filepath to dataset reference metrics.
         guide_path = None,              # Filepath to guide model.
@@ -482,13 +482,11 @@ def calculate_metrics_for_checkpoints(
         fd_metrics = True,              # Whether to calculate FID and FD-DINOv2 metrics or not
         class_metrics = True,           # Whether to calculate classification metrics or not
         chosen_emas = None,             # List of chosen EMAs. Default: use all.
-        min_epoch = None,               # Number of epochs to start processing from.
-        max_epoch = None,               # Number of epochs to stop processing at.
-        period_epoch = None,            # Sample every period_epoch epochs
         save_nimg = 0,                  # How many images to keep, the rest will be deleted.
         verbose = True,                 # Enable status prints?
         log_to_wandb = True,            # Log to W&B?
         device = torch.device("cuda"),  # Which compute device to use.
+        results_filepath = None,
         **sampler_kwargs
 ):
     
@@ -510,25 +508,8 @@ def calculate_metrics_for_checkpoints(
         if dist.get_rank() == 0:
             ref = calc.load_stats(path=ref_path) # do this first, just in case it fails
 
-    # Get available checkpoints
-    checkpoint_filenames = filter_by_string_must(os.listdir(checkpoints_dir), ".pkl")    
-    checkpoint_filenames = filter_by_string_must(checkpoint_filenames, "0000000", must=False) # Skip randomly initialized networks
-    checkpoint_emas = [abs(find_numbers(f)[-1]) for f in checkpoint_filenames]
-    if min_epoch is not None or max_epoch is not None:
-        checkpoint_filenames = np.array(checkpoint_filenames)
-        checkpoint_epochs = np.array([abs(find_numbers(f)[0]) for f in checkpoint_filenames], dtype=np.int32)
-        if period_epoch is not None:
-            keep_epochs = checkpoint_epochs%period_epoch==0
-            checkpoint_epochs = checkpoint_epochs[keep_epochs]
-            checkpoint_filenames = checkpoint_filenames[keep_epochs]
-        if min_epoch is not None:
-            checkpoint_filenames = checkpoint_filenames[checkpoint_epochs >= min_epoch]
-            checkpoint_epochs = np.array([abs(find_numbers(f)[0]) for f in checkpoint_filenames], dtype=np.int32)
-        if max_epoch is not None:
-            checkpoint_filenames = checkpoint_filenames[checkpoint_epochs <= max_epoch]
-        checkpoint_filenames = list(checkpoint_filenames)
-
     # Separate by EMA and filter EMAs, if specified
+    checkpoint_emas = [abs(find_numbers(f)[-1]) for f in checkpoint_filepaths]
     available_emas = list(set(checkpoint_emas))
     if chosen_emas is None:
         chosen_emas = available_emas
@@ -538,11 +519,11 @@ def calculate_metrics_for_checkpoints(
         chosen_emas = [ema for ema in chosen_emas if ema in available_emas]
         if len(chosen_emas)==0: 
             raise ValueError("Specified EMA/s not available")
-    checkpoint_filenames_by_ema = []
+    checkpoint_filepaths_by_ema = []
     for ema in chosen_emas:
-        these_filenames = filter_by_string_must(checkpoint_filenames, f"{ema:.3f}")
-        these_filenames.sort()
-        checkpoint_filenames_by_ema.append(these_filenames)
+        these_filepaths = filter_by_string_must(checkpoint_filepaths, f"{ema:.3f}")
+        these_filepaths.sort()
+        checkpoint_filepaths_by_ema.append(these_filepaths)
 
     # Configure sampler
     final_sampler_kwargs = EasyDict(DEFAULT_SAMPLER)
@@ -552,17 +533,24 @@ def calculate_metrics_for_checkpoints(
     # Configure distributed execution and resume W&B logging
     dist.init()
     if log_to_wandb:
-        wandb_id = get_wandb_id(checkpoints_dir)
-        if ref_path is not None: 
-            masked_ref = os.path.split(ref_path)[-1]
-        else: masked_ref = None
-        run = wandb.init(entity="ajest", project="Images", id=wandb_id, resume="allow",
-            config=dict(validation_kwargs={datetime.today().strftime('%Y-%m-%d %H:%M:%S'):dict(
-                dataset_name=dataset_name, ref_path=masked_ref, 
-                guide_path=guide_path, guidance_weight=guidance_weight,
-                class_idx=class_idx, random_class=random_class, 
-                seeds=seeds, chosen_emas=chosen_emas, **sampler_kwargs)}),
-            settings=wandb.Settings(x_disable_stats=True))
+        def open_wandb_validation_run(checkpoint_dir):
+            wandb_id = get_wandb_id(checkpoint_dir)
+            if ref_path is not None: 
+                masked_ref = os.path.split(ref_path)[-1]
+            else: masked_ref = None
+            run = wandb.init(entity="ajest", project="Images", id=wandb_id, resume="allow",
+                config=dict(validation_kwargs={datetime.today().strftime('%Y-%m-%d %H:%M:%S'):dict(
+                    dataset_name=dataset_name, ref_path=masked_ref, 
+                    guide_path=guide_path, guidance_weight=guidance_weight,
+                    class_idx=class_idx, random_class=random_class, 
+                    seeds=seeds, chosen_emas=chosen_emas, **sampler_kwargs)}),
+                settings=wandb.Settings(x_disable_stats=True))
+            return run
+        def close_wandb_validation_run(run):
+            torch.distributed.barrier()
+            try: run.finish()
+            except AttributeError: pass
+            torch.distributed.barrier()
 
     # For each EMA in chosen EMAs, and for each checkpoint inside the directory
     if fd_metrics:
@@ -570,7 +558,11 @@ def calculate_metrics_for_checkpoints(
         detectors = [calc.get_detector(metric, verbose=verbose) for metric in metrics]
     if class_metrics:
         classifier = load_classifier_model("Swin", deterministic=False)
-    for i, checkpoint_filenames in enumerate(checkpoint_filenames_by_ema):
+    if log_to_wandb: 
+        open_directory = os.path.dirname(checkpoint_filepaths_by_ema[0][0])
+        if dist.get_rank()==0: 
+            run = open_wandb_validation_run(open_directory)
+    for i, (filepaths, ema) in enumerate(zip(checkpoint_filepaths_by_ema, chosen_emas)):
         if guidance_weight!=1 and guide_path is not None:
             tag = f" [EMA={ema:.3f}, Guidance={guidance_weight:.2f}]"
         else:
@@ -581,8 +573,13 @@ def calculate_metrics_for_checkpoints(
                 wandb_logs.update({f"Validation FID"+tag: 0., f"Validation FD-DINOv2"+tag: 0.})
             if class_metrics:
                 wandb_logs.update({f"Swin-L Top-1 Accuracy"+tag: 0., f"Swin-L Top-5 Accuracy"+tag: 0.})
-        for checkpoint_filename in checkpoint_filenames:
-            checkpoint_filepath = os.path.join(checkpoints_dir, checkpoint_filename)
+        for checkpoint_filepath in filepaths:
+            checkpoint_dir = os.path.dirname(checkpoint_filepath)
+            if log_to_wandb and checkpoint_dir != open_directory:
+                close_wandb_validation_run(run)
+                open_directory = os.path.dirname(checkpoint_filepaths_by_ema[0][0])
+                if dist.get_rank()==0: 
+                    run = open_wandb_validation_run(open_directory)
             checkpoint_epochs = abs(find_numbers(checkpoint_filename)[-2])
             if log_to_wandb: wandb_logs.update({"Validation Epoch": checkpoint_epochs})
             if verbose: dist.print0(f">>>>> Working on EMA {ema:.3f} and Epoch {checkpoint_epochs}")
@@ -636,7 +633,7 @@ def calculate_metrics_for_checkpoints(
             
             # Log to W&B
             if log_to_wandb:
-                if class_metrics and checkpoint_filename==checkpoint_filenames[-1]:
+                if class_metrics and checkpoint_filename==filepaths[-1]:
                     run.log({f"Swin-L Confusion Matrix"+tag: confusion_matrix,
                              f"Swin-L Top-5 Correct"+tag: top_5_correct,
                              **wandb_logs})
@@ -653,11 +650,39 @@ def calculate_metrics_for_checkpoints(
                     for i in range(save_nimg,len(contents)): os.remove(os.path.join(temp_dir, contents[i]))
             torch.distributed.barrier()
 
-    if log_to_wandb:
-        torch.distributed.barrier()
-        try: run.finish()
-        except AttributeError: pass
-        torch.distributed.barrier()
+    if log_to_wandb: close_wandb_validation_run(run)
+
+#----------------------------------------------------------------------------
+# Calculate metrics for all stored models as a post-hoc validation curve
+
+def calculate_metrics_for_all_checkpoints(
+        checkpoints_dir,
+        min_epoch = None,               # Number of epochs to start processing from.
+        max_epoch = None,               # Number of epochs to stop processing at.
+        period_epoch = None,            # Sample every period_epoch epochs
+        **calc_metrics_kwargs
+):
+
+    # Get available checkpoints
+    checkpoint_filenames = filter_by_string_must(os.listdir(checkpoints_dir), ".pkl")    
+    checkpoint_filenames = filter_by_string_must(checkpoint_filenames, "0000000", must=False) # Skip randomly initialized networks
+    checkpoint_emas = [abs(find_numbers(f)[-1]) for f in checkpoint_filenames]
+    if min_epoch is not None or max_epoch is not None:
+        checkpoint_filenames = np.array(checkpoint_filenames)
+        checkpoint_epochs = np.array([abs(find_numbers(f)[0]) for f in checkpoint_filenames], dtype=np.int32)
+        if period_epoch is not None:
+            keep_epochs = checkpoint_epochs%period_epoch==0
+            checkpoint_epochs = checkpoint_epochs[keep_epochs]
+            checkpoint_filenames = checkpoint_filenames[keep_epochs]
+        if min_epoch is not None:
+            checkpoint_filenames = checkpoint_filenames[checkpoint_epochs >= min_epoch]
+            checkpoint_epochs = np.array([abs(find_numbers(f)[0]) for f in checkpoint_filenames], dtype=np.int32)
+        if max_epoch is not None:
+            checkpoint_filenames = checkpoint_filenames[checkpoint_epochs <= max_epoch]
+        checkpoint_filenames = list(checkpoint_filenames)
+    checkpoint_filepaths = [os.path.join(checkpoints_dir, f) for f in checkpoint_filenames]
+
+    calculate_metrics_for_checkpoints(checkpoint_filepaths, **calc_metrics_kwargs)
 
 @click.group()
 def cmdline(): pass
@@ -697,7 +722,7 @@ def metrics_generated(super_dir, fd_metrics, class_metrics, verbose, metrics_fil
 @click.option('--fd-metrics/--no-fd-metrics', 'fd_metrics',  help='Calculate FID and FD-DINOv2 metrics?', type=bool, default=True, show_default=True)
 @click.option('--class-metrics/--no-class-metrics', 'class_metrics',  help='Calculate classification metric?', type=bool, default=True, show_default=True)
 @click.option('--random/--no-random', 'random_class',  help='Use random classes?', type=bool, default=False, show_default=True)
-@click.option('--emas', help='Chosen EMA length/s', required=False, multiple=True, default=None, show_default=True)
+@click.option('--emas', help='Chosen EMA length/s', metavar='LIST', type=str, required=True, default=None, show_default=True)
 @click.option('--min-epoch', help='Number of batches at which to start', type=int, required=False, default=None, show_default=True)
 @click.option('--max-epoch', help='Number of batches at which to stop', type=int, required=False, default=None, show_default=True)
 @click.option('--period', help='Period of the number of batches to use for sampling', type=int, required=False, default=None, show_default=True)
@@ -709,9 +734,8 @@ def validation(models_dir, dataset_name, ref_path, guide_path, guidance_weight, 
     models_dir = os.path.join(dirs.MODELS_HOME, "Images", models_dir)
     if ref_path is not None: ref_path = os.path.join(dirs.DATA_HOME, "dataset_refs", ref_path)
     if guide_path is not None: guide_path = os.path.join(dirs.MODELS_HOME, "Images", guide_path)
-    if len(emas)==0: emas=None
-    else: emas = [float(ema) for ema in emas]
-    calculate_metrics_for_checkpoints(models_dir,
+    if emas is not None: emas = [float(ema) for ema in emas.split(",")]
+    calculate_metrics_for_all_checkpoints(models_dir,
         dataset_name=dataset_name, ref_path=ref_path,
         guide_path=guide_path, guidance_weight=guidance_weight,
         fd_metrics=fd_metrics, class_metrics=class_metrics,
